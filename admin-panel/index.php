1660e529657e909ca64734460010f6cb45437b52
roshanikinge <roshanikinge20@gmail.com>
2025-05-12 15:44:00 +0530

    Intial commit

---
+++ admin-panel/index.php
@@ -0,0 +1,47 @@
+
+      <div class="row">
+        <div class="col-md-3">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Countries</h5>
+              <!-- <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6> -->
+              <p class="card-text">number of countries: </p>
+             
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Cities</h5>
+              
+              <p class="card-text">number of cities: </p>
+              
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Admins</h5>
+              
+              <p class="card-text">number of admins: </p>
+              
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Bookings</h5>
+              
+              <p class="card-text">number of bookings:</p>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+   
+ <?php require "layouts/footer.php"; ?>      
